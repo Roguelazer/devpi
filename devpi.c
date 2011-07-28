7106fe1d742ad8adb1bd0110c1d126311f5fd286
James Brown <jbrown@yelp.com>
2011-07-28 16:41:54 -0700

    Clean up some stuff

--- devpi.c
+++ devpi.c
@@ -24,7 +24,6 @@ static int device_release(struct inode*, struct file*);
 static ssize_t device_read(struct file*, char* __user, size_t, loff_t);
 static ssize_t device_write(struct file*, const char*, size_t, loff_t*);
 
-static int Major;
 static int opened;
 static const char* msg = "3.141592";
 
@@ -67,41 +66,32 @@ static ssize_t device_read(struct file* filp, char* __user buffer, size_t length
 
 static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
 {
-    printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
     return -EINVAL;
-}
-
-static char *pi_devnode(struct device *dev, mode_t *mode)
-{
-    if (mode && 0666) {
-        *mode = 0666;
-    }
-    return NULL;
 }
 
 int init_module(void)
 {
+    int err;
     printk(KERN_INFO "Entering init_module");
-    Major = register_chrdev(PI_MAJOR, DEVICE_NAME, &fops);
+    err = register_chrdev(PI_MAJOR, DEVICE_NAME, &fops);
 
-    if (Major < 0) {
-        printk(KERN_ALERT "Registering char device failed with %d\n", Major);
-        return Major;
-    }
+    if (err < 0)
+        printk(KERN_ALERT "Registering char device failed with %d\n", err);
+
     pi_class = class_create(THIS_MODULE, "pi");
 
     if (IS_ERR(pi_class))
         return PTR_ERR(pi_class);
 
-    pi_class->devnode = pi_devnode;
     device_create(pi_class, NULL, MKDEV(PI_MAJOR, 1), NULL, "pi");
     return 0;
 }
 
 void cleanup_module(void)
 {
+    printk(KERN_INFO "Entering cleanup_module");
     device_destroy(pi_class, MKDEV(PI_MAJOR, 1));
-    unregister_chrdev(Major, DEVICE_NAME);
+    unregister_chrdev(PI_MAJOR, DEVICE_NAME);
     class_destroy(pi_class);
 }
 
