70a38a5b561f8d735a84b108c552182851a320a2
James Brown <jbrown@yelp.com>
2011-07-29 13:43:41 -0700

    import evan's pihex stuff, which doesn't work yet

--- devpi.c
+++ devpi.c
@@ -13,10 +13,12 @@
 #include <asm/segment.h>
 #include <linux/mm.h>
 #include <linux/slab.h>
+#include <asm/i387.h>
 #include "devpi.h"
 
 #define MIN(a,b) ((a)>(b)?(b):(a))
 #define WRITE_SIZE 4096
+#define EPS (1e-18)
 
 /*****************************
  *
@@ -155,6 +157,88 @@ static struct file_operations fops = {
 };
 
 /******************************
+ * 
+ * Hex Pi (Bellard's Formula)
+ *
+ *****************************/
+double modpow(long base, long exponent, long modulus)
+{
+	long result = 1;
+	while (exponent > 0) {
+		if (exponent & 1) {
+			result = (result * base) % modulus;
+		}
+		exponent >>= 1;
+		base = (base * base) % modulus;
+	}
+	return (double) result;
+}
+
+double pow16(long exp) {
+	int i;
+	long pos_exp = exp < 0 ? -exp : exp;
+	double total = 1.0;
+	if (exp == 0) {
+		return 1.0;
+	}
+	for (i = 0; i < pos_exp; i++) {
+		total *= 16.0;
+	}
+	if (exp < 0) {
+		total = 1 / total;
+	}
+	return total;
+}
+
+double compute_term(long n, long j)
+{
+	double s = 0.0, t = 0.0;
+	double newt;
+	double pow;
+	double diff;
+	long k = 0;
+	long r;
+
+	/* left sum */
+	while (k <= n) {
+		r = 8*k + j;
+		s = s + modpow(16, n-k, r) / r;
+		s -= (long) s; /* fmod by 1.0 */
+		k++;
+	}
+
+	/* right sum */
+	k = n + 1;
+	while (1) {
+		pow = pow16(n-k);
+		newt = t + pow / ((k << 3) + j);
+		diff = t - newt;
+		diff = diff < 0 ? -diff : diff;
+		if (diff < EPS) {
+			break;
+		} else {
+			t = newt;
+		}
+		k++;
+	}
+	return s + t;
+}
+
+static int compute_digit(long n) {
+	double s;
+    s = 4.0 * compute_term(n, 1);
+    s -= 2.0 * compute_term(n, 4);
+    s -= compute_term(n, 5);
+    s -= compute_term(n, 6);
+
+	/* fmod s by 1.0 */
+	while (s < 0) { s++; }
+	while (s > 1) { s--; }
+
+	return s * 16;
+}
+
+/******************************
  *
  * Actual implementation
  *
@@ -269,6 +353,18 @@ static ssize_t device_read(struct file* filp, char __user * buffer, size_t lengt
             }
             i = (i + 1) % NUM_PIES;
         }
+    } else if (mode == HEX) {
+        int i;
+        orig_cbuf = kmalloc(length + 1, GFP_USER);
+        if (orig_cbuf == NULL) {
+            printk(KERN_WARNING "cbuf allocation failed\n");
+            return -ENOMEM;
+        }
+        kernel_fpu_begin();
+        for (i = 0; i < length; ++i) {
+            snprintf(cptr+i, 1, "%x", compute_digit(i));
+        }
+        kernel_fpu_end();
     }
     cptr = orig_cbuf;
     printk(KERN_INFO "Copying to userspace from 0x%p\n", cptr);
